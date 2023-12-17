032815b4bd5d010d3c98e086c06222253df95863
Caio Keven <caiokeven3176@gmail.com>
2023-12-17 01:53:56 -0300

    edit face

--- engine.py
+++ engine.py
@@ -1,37 +1,35 @@
 import face_recognition as fr
-import numpy as np
 import cv2
 import os
-faces=[]
 
-def facial_recognition ():
+faces = []
+
+
+def facial_recognition():
     take_picture()
-    foto_data= fr.load_image_file("./img/picture.jpeg")
-    faces1=fr.face_encodings(foto_data)
-    if len(faces1)>0:
+    foto_data = fr.load_image_file("./img/picture.jpeg")
+    faces1 = fr.face_encodings(foto_data)
+    if len(faces1) > 0:
         return True, faces1
     return False, []
 
+
 def new_face(nome):
-    new_face=facial_recognition()
+    new_face = facial_recognition()
     if new_face[0]:
-        new_face_data=[nome,new_face[1]]
+        new_face_data = [nome, new_face[1]]
         faces.append(new_face_data)
-        print('Rosto do ',nome, 'cadastrado com sucesso')
+        print("Rosto do ", nome, "cadastrado com sucesso")
     else:
-        print('Rosto não cadastrado')
-    
+        print("Rosto não cadastrado")
 
 
 def identify_face():
-    face_test = facial_recognition()  # Assuming the captured face is in the most recent image
-    
+    face_test = facial_recognition()
     if face_test[0]:
         for known_face in faces:
             for datas in known_face[1]:
-                # Compare the face encodings
                 match = fr.compare_faces([datas], face_test[1][0])
-
                 if any(match):
                     print(f"Rosto identificado como  {known_face[0]}")
                     return
@@ -39,32 +37,65 @@ def identify_face():
     else:
         print("Não há rostos na imagem")
 
-    
-    
 
 def take_picture():
-    cam=cv2.VideoCapture(0)
-    
+    cam = cv2.VideoCapture(0)
     while True:
-        picture,frame = cam.read()
-        cv2.imshow("Camera",frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        picture, frame = cam.read()
+        cv2.imshow("Camera", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             cam.release()
             cv2.destroyAllWindows()
             break
     if picture:
         if not os.path.exists("img"):
             os.makedirs("img")
-
-        # Define o caminho do arquivo de imagem
         caminho_arquivo = os.path.join("img", "picture.jpeg")
-
-        # Salva a última imagem no formato JPEG antes de encerrar
         cv2.imwrite(caminho_arquivo, frame)
-        print(f"Foto salva em: {caminho_arquivo}")
     else:
         print("Não foi possível capturar a imagem.")
 
-new_face("caio")
 
-identify_face()
+def edit_face():
+    if not faces:
+        print("Nenhum rosto cadastrado para editar.")
+        return
+    name = input("Digite o nome da pessoa que  deseja editar: ")
+    for i, face_data in enumerate(faces):
+        if face_data[0] == name:
+            new_name = input("Digite o novo nome ): ")
+            if new_name:
+                face_data[0] = new_name
+            print("Informações editadas com sucesso.")
+            return
+    print("Rosto não encontrado. Certifique-se de que o nome está correto.")
+
+
+def show_names():
+    if not faces:
+        print("Nenhum rosto cadastrado.")
+    else:
+        print("Rostos cadastrados:")
+        for face_data in faces:
+            print(face_data[0])
+
+
+n1 = int
+while n1 != "5":
+    print("---------------------------------------------------")
+    n1 = input(
+        "O que deseja fazer?\n1-Cadastrar um rosto\n2-Verificar se um rosto está cadastrado\n3-Editar as informações de uma pessoa\n4-Listar os rostos cadastrados\n5-Sair\n"
+    )
+    if n1 == "1":
+        new_face(input("Digite o nome da pessoa que deseja cadastrar: "))
+    elif n1 == "2":
+        identify_face()
+    elif n1 == "3":
+        edit_face()
+    elif n1 == "4":
+        show_names()
+    elif n1 == "5":
+        print("Saindo do programa. Até mais!")
+        break
+    else:
+        print("Opção inválida. Por favor, escolha uma opção válida.")
